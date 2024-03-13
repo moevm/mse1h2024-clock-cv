aa047a3f42aa0dfafbf6f5425401d0c105ef89fe
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-03-13 19:20:52 +0300

    init frontend

---
+++ mse1h2024-clock-cv-frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap')
+
+.container {
+  background-image: url('./assets/fon.jpg');
+}
+</style>
