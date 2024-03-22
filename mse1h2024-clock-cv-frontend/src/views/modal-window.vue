e25b2a6cef327c55ed6475c78e84084fb19ec05c
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-03-22 18:43:31 +0300

    fix name directory

---
+++ mse1h2024-clock-cv-frontend/src/views/modal-window.vue
@@ -0,0 +1,129 @@
+<script>
+  export default {
+    name: 'ModalWindow',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header
+          class="modal-header"
+          id="modalTitle"
+        >
+          <slot name="header">
+            <p class="text4">Количество баллов:<br/><span class="number1"><!--Здесь количество баллов за тест--></span></p>
+
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              <img src="../assets/close.png" class="closeknop">
+            </button>
+          </slot>
+        </header>
+        <section
+          class="modal-body"
+          id="modalDescription"
+        >
+          <slot name="body">
+            I'm the default body!
+            <p class="text5">Здесь текст пояснения баллов</p>
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+<style scoped>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+  }
+
+  .modal {
+    width: 1097px; 
+    height: 863px; 
+    background: #FFFCFC; 
+    border-radius: 20px; 
+    border: 8px #6FD9CD solid;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+
+  .modal-header {
+    display: flex;
+    flex-direction: row;
+    color: #4AAE9B;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    border: none;
+    font-size: 20px;
+    margin-left: auto;
+    margin-right: 2%;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+  .closeknop{
+    width: 88px; 
+    height: 90px;
+  }
+
+  .text4{
+    color: black; 
+    font-size: 50px; 
+    font-family: Comfortaa; 
+    font-weight: 400; 
+    word-wrap: break-word;
+    margin-left: 20%;
+  }
+
+  .text5{
+    text-align: justify; 
+    color: black; 
+    font-size: 50px; 
+    font-family: Comfortaa; 
+    font-weight: 700; 
+    word-wrap: break-word;
+    margin-left: 4%;
+  }
+
+  .number1{
+    color: black; 
+    font-size: 80px; 
+    font-family: Comfortaa; 
+    font-weight: 400; 
+    word-wrap: break-word;
+  }
+</style>
