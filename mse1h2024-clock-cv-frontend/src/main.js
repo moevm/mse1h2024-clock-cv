4113e85a076a99577324d5ef29e8a444acaed8a6
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-03-25 17:13:30 +0300

    Update main.js

--- mse1h2024-clock-cv-frontend/src/main.js
+++ mse1h2024-clock-cv-frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import axios from "axios";
+import store from "./store"
 
-createApp(App).use(router).mount('#app')
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.mount('#app')
