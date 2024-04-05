5079c4256d4a00b4dbc954afa7d814a5f7d2fd4e
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-04-05 12:34:28 +0300

    registration page design

--- mse1h2024-clock-cv-frontend/src/router.js
+++ mse1h2024-clock-cv-frontend/src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Login from './views/Login.vue';
 import Result from './views/Result.vue';
 import Loading from "./views/Loading.vue";
-//import Registr from "./views/Registr.vue";
+import Registr from "./views/Registr.vue";
 //import Recovery from './views/Recovery.vue'
 //import History from './views/History.vue'
 
@@ -11,9 +11,9 @@ import Loading from "./views/Loading.vue";
 const routes = [
     { path: '/', name: 'Login', component: Login },
     { path: '/loading', name: 'Loading', component: Loading},
-    { path: '/result', name: 'Result', component: Result},/*
-    { path: '/registr', name: 'Registr', component: Registr},
-    { path: '/history', name: 'History', component: History},*//*
+    { path: '/result', name: 'Result', component: Result},
+    { path: '/registr', name: 'Registr', component: Registr}/*,
+    { path: '/history', name: 'History', component: History},
     { path: '/recovery', name: 'Recovery', component: Recovery}*/
 ]
 
