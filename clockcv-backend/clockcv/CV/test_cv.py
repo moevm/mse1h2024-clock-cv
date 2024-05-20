afdfe69437bde5b645eb08a4edfc3b8a27c7ef34
Dariiiii <usadariaa@yandex.ru>
2024-05-20 16:37:28 +0300

    fix_import2

--- clockcv-backend/clockcv/CV/test_cv.py
+++ clockcv-backend/clockcv/CV/test_cv.py
@@ -1,5 +1,5 @@
 import os
-from .solver import CVSolver, cv2, elementFinder
+from solver import CVSolver, cv2, elementFinder
 
 def non_async_startup(filename,time):
     image = cv2.imread(filename)
