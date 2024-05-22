1f86872d7136acd9c8924cc54b69ae187c7fefd3
Корякин Айсен <koryakinbezlimit@gmail.com>
2024-05-22 19:36:19 +0300

    selenium tests

---
+++ tests/basic_test.py
@@ -0,0 +1,49 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+class TestGuestLoginUploadPhoto(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://localhost:8080/#/")
+
+    def test_guest_login_upload_photo(self):
+        # Нажимаем на кнопку "Гостевой вход"
+        guest_button = self.driver.find_element(By.XPATH, "//a[contains(@href, '#/loading')]")
+        guest_button.click()
+
+        # Нажимаем на кнопку "Загрузить фото"
+        upload_photo_button = self.driver.find_element(By.XPATH, "//button[@class='upload-button']")
+        upload_photo_button.click()
+
+        # Загружаем фото
+        file_path = os.path.join(os.path.dirname(__file__), 'sample.png')
+        file_input = self.driver.find_element(By.XPATH, "//input[@type='file' and @id='upload-input']")
+        file_input.send_keys(file_path)
+
+        load_photo_button = self.driver.find_element(By.XPATH, "//button[@id='result-button']")
+        load_photo_button.click()
+
+        expected_url = "http://localhost:8080/#/result"
+        try:
+            WebDriverWait(self.driver, 10).until(EC.url_to_be(expected_url))
+            print("Правильный URL: {}".format(self.driver.current_url))
+        except TimeoutException:
+            print("Тайм-аут. Правильный URL не был достигнут.")
+
+        # Проверяем содержимое контейнера с результатом
+        container_content = self.driver.find_element(By.XPATH, "//span[@class='number']")
+        self.assertTrue(1 <= int(container_content.text) <= 10, "Содержимое контейнера не является числом от 1 до 10")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
